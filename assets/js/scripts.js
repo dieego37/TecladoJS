eef96ae1c06c28fce24eca62dae85eb79e168a19
dieego37 <dieego37@hotmail.com>
2021-03-17 20:00:29 -0500

    primer commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,194 @@
+//elementos
+let teclasPr = document.getElementById("teclasPr");
+
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+
+let btnQ = document.getElementById("btnQ");
+let btnW = document.getElementById("btnW");
+let btnE = document.getElementById("btnE");
+let btnR = document.getElementById("btnR");
+let btnT = document.getElementById("btnT");
+let btnY = document.getElementById("btnY");
+let btnU = document.getElementById("btnU");
+let btnI = document.getElementById("btnI");
+let btnO = document.getElementById("btnO");
+let btnP = document.getElementById("btnP");
+
+let btnA = document.getElementById("btnA");
+let btnS = document.getElementById("btnS");
+let btnD = document.getElementById("btnD");
+let btnF = document.getElementById("btnF");
+let btnG = document.getElementById("btnG");
+let btnH = document.getElementById("btnH");
+let btnJ = document.getElementById("btnJ");
+let btnK = document.getElementById("btnK");
+let btnL = document.getElementById("btnL");
+let btnÑ = document.getElementById("btnÑ");
+
+let btnZ = document.getElementById("btnZ");
+let btnX = document.getElementById("btnX");
+let btnC = document.getElementById("btnC");
+let btnV = document.getElementById("btnV");
+let btnB = document.getElementById("btnB");
+let btnN = document.getElementById("btnN");
+let btnM = document.getElementById("btnM");
+
+
+
+//eventos click
+btnNum1.onclick = function () {
+    console.log("1");
+}
+
+btnNum2.onclick = function () {
+    console.log("2");
+}
+
+btnNum3.onclick = function () {
+    console.log("3");
+}
+
+btnNum4.onclick = function () {
+    console.log("4");
+}
+
+btnNum5.onclick = function () {
+    console.log("5");
+}
+
+btnNum6.onclick = function () {
+    console.log("6");
+}
+
+btnNum7.onclick = function () {
+    console.log("7");
+}
+
+btnNum8.onclick = function () {
+    console.log("8");
+}
+
+btnNum9.onclick = function () {
+    console.log("9");
+}
+
+btnNum0.onclick = function () {
+    console.log("0");
+}
+
+btnQ.onclick = function () {
+    console.log("Q");
+}
+
+btnW.onclick = function () {
+    console.log("W");
+}
+
+btnE.onclick = function () {
+    console.log("E");
+}
+
+btnR.onclick = function () {
+    console.log("R");
+}
+
+btnT.onclick = function () {
+    console.log("T");
+}
+
+btnY.onclick = function () {
+    console.log("Y");
+}
+
+btnU.onclick = function () {
+    console.log("U");
+}
+
+btnI.onclick = function () {
+    console.log("I");
+}
+
+btnO.onclick = function () {
+    console.log("O");
+}
+
+btnP.onclick = function () {
+    console.log("P");
+}
+
+btnA.onclick = function () {
+    console.log("A");
+}
+
+btnS.onclick = function () {
+    console.log("S");
+}
+
+btnD.onclick = function () {
+    console.log("D");
+}
+
+btnF.onclick = function () {
+    console.log("F");
+}
+
+btnG.onclick = function () {
+    console.log("G");
+}
+
+btnH.onclick = function () {
+    console.log("H");
+}
+
+btnJ.onclick = function () {
+    console.log("J");
+}
+
+btnK.onclick = function () {
+    console.log("K");
+}
+
+btnL.onclick = function () {
+    console.log("L");
+}
+
+btnÑ.onclick = function () {
+    console.log("Ñ");
+}
+
+btnZ.onclick = function () {
+    console.log("Z");
+}
+
+btnX.onclick = function () {
+    console.log("X");
+}
+
+btnC.onclick = function () {
+    console.log("C");
+}
+
+btnV.onclick = function () {
+    console.log("V");
+}
+
+btnB.onclick = function () {
+    console.log("B");
+}
+
+btnN.onclick = function () {
+    console.log("N");
+}
+
+btnM.onclick = function () {
+    console.log("M");
+}
